2d93cb2903b8a5529d099da2b73ada416ff3c126
Jonathan Chang <jonathanmchang@gmail.com>
2017-12-19 14:36:10 -0500

    defined vets model

---
+++ server/db/models/vet.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Vet = db.define('vet', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.String,
+    allowNull: false,
+    unique: true
+  },
+  description: {
+    type: Sequelize.String,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'https://vignette.wikia.nocookie.net/simpsons/images/5/5e/Dr._Riviera.png/revision/latest?cb=20120509073411'
+  }
+});
+
+module.exports = Vet;
