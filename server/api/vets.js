0bfa390ac557f89b4b8fb6b92b19571c79e25494
Jonathan Chang <jonathanmchang@gmail.com>
2017-12-19 15:01:40 -0500

    added getOne vet and post Vet routes -> tested in postman

--- server/api/vets.js
+++ server/api/vets.js
@@ -8,3 +8,27 @@ router.get('/', (req, res, next) => {
   .then(vets => res.json(vets))
   .catch(next)
 });
+
+router.get(`/:vetId`, (req, res, next) => {
+  Vet.findOne({
+    where: {
+      id: Number(req.params.vetId)
+    }
+  })
+  .then(vet => res.json(vet))
+  .catch(next)
+});
+
+router.post('/', (req, res, next) => {
+  let data = req.body
+  Vet.findOrCreate({
+    where: {
+      name: data.name,
+      email: data.email,
+      description: data.description,
+      imageUrl: data.imageUrl
+    }
+  })
+  .then(vet => res.json(vet))
+  .catch(next)
+});
