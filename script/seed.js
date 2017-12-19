d82ebdc12a7d848ff8d48846cce258bec8f537c6
Jonathan Chang <jonathanmchang@gmail.com>
2017-12-19 14:53:41 -0500

    added vets to seed file

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,34 @@
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const { User, Vet } = require('../server/db/models')
 
 async function seed () {
   await db.sync({force: true})
   console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
+
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'test@test.com', password: '123'}),
+    User.create({email: 'test2@test.com', password: '123'})
   ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
+
+  const organisms = await Promise.all([
+    Vet.create({
+      name: 'Vet 1',
+      email: 'vet@vet.com',
+      description: "I've been a vet for 10+ years"
+    }),
+    Vet.create({
+      name: 'Dog Vet',
+      email: 'dogvet@vet.com',
+      description: "I've been treating dogs forever"
+    }),
+    Vet.create({
+      name: 'Cat Vet',
+      email: 'cat@vet.com',
+      description: 'I just finished vet school'
+    }),
+  ]);
+
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
